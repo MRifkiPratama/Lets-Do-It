36585603a6a4da4e0431f082e91ffa78d39b21cf
MRifkiPratama <133479361+MRifkiPratama@users.noreply.github.com>
2024-06-04 18:04:36 +0700

    Add files via upload
    sql_db.sql update

--- Backend/sql_db.sql
+++ Backend/sql_db.sql
@@ -7,3 +7,44 @@ CREATE TABLE users (
     password VARCHAR NOT NULL,
     role_id INTEGER
 );
+
+CREATE TYPE task_status AS ENUM (
+	'not_started', 
+	'in_progress', 
+	'completed'
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    name VARCHAR NOT NULL,
+    detail VARCHAR,
+    due_date TIMESTAMP,
+    status task_status NOT NULL DEFAULT 'not_started',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE roles (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE role_permission (
+	id INT PRIMARY KEY NOT NULL,
+	role_id INT NOT NULL,
+	permission_id INT NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE permission (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR NOT NULL,
+	description VARCHAR NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
